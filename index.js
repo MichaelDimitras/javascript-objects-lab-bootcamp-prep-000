b97e4461814964a0e20346ad45110020eaf02438
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 20:07:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var recipes = {};
 function updateObjectWithKeyAndValue(obj, k, v){
   return obj.assign(k,v);
 }
+
+
+function destructivelyUpdateObjectWithKeyAndValue(o,k,v){
+  o[k] = v;
+  return o;
+}
